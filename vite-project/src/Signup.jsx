997ffb3127b858db9919539fe83a89e6d837f0f2
Sandhya <144054124+Sandhya015@users.noreply.github.com>
2024-05-31 00:37:43 +0530

    Add files via upload

---
+++ vite-project/src/Signup.jsx
@@ -0,0 +1,39 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Card from '@mui/material/Card';
+
+
+
+function Signup(){
+    return (
+        <div>
+            <center>
+                <div style={{
+                    marginTop:150,marginBottom:10,
+                }}>
+                    Welcome to To-Do-App, Signup here
+                </div>
+            </center>
+            <center>
+               <Card variant={'outlined'} style={{width:500,padding:20}}>
+                <div style={{
+                    width: 400,
+                    padding:20,
+                    border:"2px solid black",
+                    marginTop:10,
+                    marginBottom:10,
+                }}>
+                   
+                 <TextField fullWidth id="outlined-basic" label="Email" variant="outlined" />
+                 <br /> <br />
+                 <TextField fullWidth id="outlined-basic" label="Password" variant="outlined" type={"password"}/>
+                    <br/> <br />
+                    <Button size ={"large"}variant="contained">Hello world</Button>
+                </div>
+                </Card>
+            </center>
+        </div>
+    );
+}
+
+export default Signup;
